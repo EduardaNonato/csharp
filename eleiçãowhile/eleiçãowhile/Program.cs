19bb40173c3873e2f1b5d1acede8bdae4e8f3bc6
EduardaNonato <115711892+EduardaNonato@users.noreply.github.com>
2022-10-11 19:45:37 -0300

    eleição

---
+++ eleiçãowhile/eleiçãowhile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eleiçãowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
